ee372f655f5fcd65a2af15ef713a30da329da1ff
Kristian Duda <kristianduda83@gmail.com>
2022-10-15 15:58:47 +0200

    schema

--- prisma/migrations/20221015135802_init/migration.sql
+++ prisma/migrations/20221015135802_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "permissions" BIGINT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
