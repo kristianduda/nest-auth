6e2169e8cc2bf3cafb66c599b317c439fcb953d6
Kristian Duda <kristianduda83@gmail.com>
2022-11-26 11:00:07 +0100

    version

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
   const config = new DocumentBuilder()
-    .setTitle('Auth')
+    .setTitle('auth')
     .setDescription('jwt auth using nest.js')
-    .setVersion('1.0')
+    .setVersion('0.1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
