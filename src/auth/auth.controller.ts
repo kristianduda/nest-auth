8ff65e8db75fa570fd9239510f6bf8c26c616096
Kristian Duda <kristianduda83@gmail.com>
2022-11-28 14:27:59 +0100

    login dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,29 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
-import { AuthService } from './auth.service';
-import { CurrentUser } from './decorators/currentUser.decorator';
-import { TokenDto } from './dto/token.dto';
-import { LocalAuthGuard } from './guards/local-auth.guard';
-import { RefreshAuthGuard } from './guards/refresh-auth.guard';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
+import { LoginDto } from 'src/auth/dto/login.dto'
+import { AuthService } from './auth.service'
+import { CurrentUser } from './decorators/currentUser.decorator'
+import { TokenDto } from './dto/token.dto'
+import { LocalAuthGuard } from './guards/local-auth.guard'
+import { RefreshAuthGuard } from './guards/refresh-auth.guard'
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiBody({ type: LoginDto })
   @ApiCreatedResponse({ type: TokenDto })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    return this.authService.login(req.user)
   }
 
   @ApiCreatedResponse({ type: TokenDto })
-  @UseGuards(RefreshAuthGuard) 
+  @UseGuards(RefreshAuthGuard)
   @Post('refresh')
   async refresh(@CurrentUser() currentUser) {
-    return await this.authService.refresh(currentUser);
+    return await this.authService.refresh(currentUser)
   }
 }
