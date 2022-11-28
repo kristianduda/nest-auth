5a6072d5adaf9bf8ed92fa45d564a157abf8cf4f
Kristian Duda <kristianduda83@gmail.com>
2022-11-28 08:19:29 +0100

    token dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/currentUser.decorator';
+import { TokenDto } from './dto/token.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { RefreshAuthGuard } from './guards/refresh-auth.guard';
 
@@ -10,12 +11,14 @@ import { RefreshAuthGuard } from './guards/refresh-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiCreatedResponse({ type: TokenDto })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
+  @ApiCreatedResponse({ type: TokenDto })
   @UseGuards(RefreshAuthGuard) 
   @Post('refresh')
   async refresh(@CurrentUser() currentUser) {
